7d14b61dde67a113264954ae1f1de61b3ca7aca5
Kevin Curtis <kevin@k-curtis.com>
2021-03-23 20:55:32 -0500

    testing fetch

---
+++ build/precache-manifest.f6e0086a79a5e3d05b45271db2dafd4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f67de7a5971cead5f2019384e285e141",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7a833b56564d76af61e",
+    "url": "/static/css/main.1f67552e.chunk.css"
+  },
+  {
+    "revision": "0356968abf233cca164e",
+    "url": "/static/js/2.7cfc0213.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7cfc0213.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7a833b56564d76af61e",
+    "url": "/static/js/main.3362eeb9.chunk.js"
+  },
+  {
+    "revision": "6e8707f7dc570b0bbca5",
+    "url": "/static/js/runtime-main.af5b3722.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
